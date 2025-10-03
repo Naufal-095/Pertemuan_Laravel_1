4466cc493b56b05f57e96f67878cc9bae62ea434
Muhammad Naufal Dwiantomo <naufaldwiantomo@gmail.com>
2025-10-03 14:31:12 +0700

    feat: Larapress

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+    <h1>Selamat Datang di kontak LaraPress</h1>
+    <p>Email=naufal4523076@univpancasila.ac.id</p>
+    <p>NoTelp=085591421581</p>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
